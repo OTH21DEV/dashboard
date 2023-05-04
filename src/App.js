07ed32400a7d1fd7ec07f121ec46270623a716bc
Oxana Theis <otheis@protonmail.com>
2023-05-04 12:32:42 +0100

    workers and exec charts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,109 @@
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
 import PlaybookStats from "./components/PlaybooksStats/PlaybookStats";
+import './app.css'
+import axios from "axios";
+axios.defaults.headers = {
+  statuskey: "sebuxor",
+};
 
+let executionLegend={
+  first:'total executions',
+  second:"today's executions"
+}
+let workerLegend={
+  first:'worker count',
+  second:"consumer count"
+}
 function App() {
- 
+  const [playbooksExecutions, setPlaybooksExecutions] = useState([]);
+  const [worker, setWorker] = useState([]);
+  const url = "https://purplecore.xyz/api/v1/management/playbooks/activity/stats";
+
+  const getStats = async () => {
+    const result = await axios.get(url);
+    console.log(result);
+
+
+    let counterExecution ={
+      first:result.data.data.executionsTotal,
+      second:result.data.data.executionsToday,
+    };
+
+
+    let counterWorker={
+      first: result.data.data.workerCount,
+      second:result.data.data.publicServers[0].consumerCount
+      // second: result.data.data.workerCount,
+    }
+    localStorage.setItem("counterExecution", JSON.stringify(counterExecution));
+    localStorage.setItem("counterWorker", JSON.stringify(counterWorker));
+
+
+    localStorage.setItem("maxExecutionLimit", result.data.data.maxExecutionLimit);
+
+    setPlaybooksExecutions([
+      {
+        id: "Todays executions",
+        data: [
+          {
+            x: "Today executions",
+
+            y: result.data.data.executionsToday,
+          },
+        ],
+      },
+
+      {
+        id: "Total executions",
+        data: [
+          {
+            x: "Total executions",
+            y: result.data.data.executionsTotal,
+          },
+        ],
+      },
+    ]);
+
+    setWorker([
+      {
+        id: "Worker Count",
+        data: [
+          {
+            x: "Worker Count",
+
+            y: result.data.data.workerCount,
+          },
+        ],
+      },
+
+      {
+        id: "Consumer Count",
+        data: [
+          {
+            x: "Consumer Count",
+            y: result.data.data.publicServers[0].consumerCount,
+          },
+        ],
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    getStats();
+  }, []);
+
+
+
+
+
   return (
     <div className="App">
-     <Header></Header>
-     <PlaybookStats></PlaybookStats>
-
+      <Header></Header>
+      <section className="radial-charts">
+      <PlaybookStats legend={executionLegend}counter={JSON.parse(localStorage.getItem('counterExecution'))}maxValue={localStorage.getItem("maxExecutionLimit")} data={playbooksExecutions} title="Playbooks Activity Stats"></PlaybookStats>
+      <PlaybookStats  legend={workerLegend}counter={JSON.parse(localStorage.getItem('counterWorker'))}maxValue={150} data={worker} title="Threatconnect Engine Metrics"></PlaybookStats>
+      </section>
     </div>
   );
 }
--- src/components/PlaybooksStats/PlaybookStats.jsx
+++ src/components/PlaybooksStats/PlaybookStats.jsx
@@ -1,124 +1,21 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { ResponsiveRadialBar } from "@nivo/radial-bar";
 
 import "./playbookStats.css";
-import GetData from "../../services/services";
+
 import axios from "axios";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
 axios.defaults.headers = {
   statuskey: "sebuxor",
 };
 
-const PlaybookStats = () => {
-  const dotToday = <FontAwesomeIcon icon={faCircle} color={'#e8c1a0'} />
-  const dotTotal = <FontAwesomeIcon icon={faCircle} color={'#f47560'} />
-  const [playbooksExecutions, setPlaybooksExecutions] = useState([]);
-
-  const url = "https://purplecore.xyz/api/v1/management/playbooks/activity/stats";
-
-  const getStats = async () => {
-    const result = await axios.get(url);
-    console.log(result);
-
-  
-
-    localStorage.setItem("executionsToday", result.data.data.executionsToday);
-    localStorage.setItem("executionsTotal", result.data.data.executionsTotal);
-
-    setPlaybooksExecutions([
-      {
-        id: "Todays executions",
-        data: [
-          {
-            x: "Today executions",
-
-            y: result.data.data.executionsToday,
-          },
-        ],
-      },
-
-      {
-        id: "Total executions",
-        data: [
-          {
-            x: "Total executions",
-            y: result.data.data.executionsTotal,
-          },
-        ],
-      },
-    ]);
-  };
-  // console.log(playbooksExecutions.length)
-
-  useEffect(() => {
-    getStats();
-  }, []);
-
- 
-  // console.log(playbooksExecutions[0].data[0].y)
-
-  //////////////////////////////////:V2
-
-  // useEffect(() => {
-  //   const stats = new GetData();
-
-  //   stats.getData().then( async() => {
-  //     // setExecutionsTotal([
-  //     //   {
-  //     //     id: "Total executions",
-  //     //     ranges: [],
-  //     //     measures: test.playbooksData.executionsTotal,
-  //     //     markers: [test.playbooksData.executionsTotal],
-  //     //   },
-  //     // ]);
-  //     // setExecutionsToday([
-  //     //   {
-  //     //     id: "Today's executions ",
-  //     //     ranges: [0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000],
-  //     //     measures: test.playbooksData.executionsToday,
-  //     //     markers: [test.playbooksData.executionsToday],
-  //     //   },
-  //     // ]);
-
-  //    setPlaybooksExecutions([
-  //       {
-  //         id: "Todays executions",
-  //         data: [
-  //           {
-  //             x: "Today executions",
-
-  //             y: stats.playbooksData.executionsToday,
-  //           },
-
-  //         ],
-  //       },
-
-  //       {
-  //         id: "Total executions",
-  //         data: [
-  //           {
-  //             x: "Total executions",
-  //             y: stats.playbooksData.executionsTotal,
-  //           },
-  //         ],
-  //       },
-  //       // {
-  //       //   id: "Execution limits",
-  //       //   data: [
-  //       //     {
-  //       //       x: "Execution limits",
-  //       //       y: 10000,
-  //       //     },
-  //       //   ],
-  //       // },
-  //     ]);
-
-  //   });
-
-  // }, []);
+const PlaybookStats = ({ title, data, maxValue, counter, legend }) => {
+  console.log(counter);
+  const dotToday = <FontAwesomeIcon icon={faCircle} color={"#e8c1a0"} fontSize={"10px"} />;
+  const dotTotal = <FontAwesomeIcon icon={faCircle} color={"#f47560"} fontSize={"10px"} />;
 
   const theme = {
     // background: "#222222",
@@ -194,132 +91,111 @@ const PlaybookStats = () => {
 
   return (
     <div className="bullet-container">
-      <h2>Playbooks Activity Stats</h2>
-      {/* <div className="test"> */}
-      {/* <ResponsiveBullet
-        data={executionsTotal}
-        margin={{ top: 50, right: 90, bottom: 50, left: 190 }}
-        spacing={46}
-        titleAlign="start"
-        titleOffsetX={-120}
-        measureSize={0.2}
-        rangeColors={"seq:yellow_green"}
-        measureBorderWidth={"13px"}
-        theme={theme}
-        axisBottom={axisBottom}
-        legends={legends}
-        tickValues={[0, 10000]}
-        animate={true}
-        maxValue={10000}
-      />
-  
-      <ResponsiveBullet
-        data={executionsToday}
-        margin={{ top: 50, right: 90, bottom: 50, left: 190 }}
-        spacing={46}
-        titleAlign="start"
-        titleOffsetX={-120}
-        measureSize={0.2}
-        rangeColors={"seq:orange_red"}
-        measureBorderWidth={"13px"}
-      /> */}
-<div className="chart-wrapper">
-      <ResponsiveRadialBar
-        data={playbooksExecutions}
-        // centerValue={{
-        //   value: 2500,
-        //   formatted: true
-        // }}
-        // legendLabel={(x)=>${x.value}}
-        //  valueFormat="0=-0.0%"
-        // tooltip={(data) => <div className="tooltip">{data.bar.data.y}</div>}
-        maxValue={10000}
-        startAngle={-90}
-        endAngle={90}
-        // endAngle={275}
-        innerRadius={0.25}
-        padding={0.4}
-        padAngle={0}        
-        margin={{ top: 40 }}
-      
-        // margin={{ top: 40, right: 120, bottom: 40, left: 40 }}
-        colors={{ scheme: "nivo" }}
-        borderColor={{
-          from: "color",
-          modifiers: [["darker", "25"]],
-        }}
-        //   theme={{
-        //     dots: {
-        //         text: {
-        //             fill: '#ffffff',
-        //         },
-        //     },
-        // }}
-        enableTracks={true}
-        //legende of bars
-        // radialAxisStart={{ tickSize: 20, tickPadding: 9, tickRotation: 0 }}
-        // circularAxisOuter={{ tickSize: 5, tickPadding: 5, tickRotation: 0 }}
-        enableLabels={false}
-        label="value"
-        labelsTextColor={{ theme: "labels.text.fill" }}
-        enableRadialGrid={false}
-        enableCircularGrid={false}
-        radialAxisStart={false}
-        motionConfig="gentle"
-        isInteractive={true}
-        theme={theme}
-        // tooltip={({ bar }) => {
-        //   return (
-        //     <BasicTooltip
-        //       value={`${bar.value}`}
-        //       color={bar.color}
-        //       id={
-        //         <span>
-        //           {bar.category} - {bar.groupId}
-        //         </span>
-        //       }
-        //       enableChip
-        //     />
-        //   );
-        // }}
+      <h2>{title}</h2>
 
-        layers={["grid", "tracks", "bars", "labels", "legends", "CenteredMetric"]}
-        // legends={[
-        //   {
-        //     dataFrom: "keys",
-        //     // data: keys.map((item,index)=>({
-        //     //   id: item.id,
-        //     //   label:item.y
-        //     // })),
-        //     anchor: "right",
-        //     direction: "column",
-        //     justify: false,
-        //     translateX: 60,
-        //     //  translateY: -17,
-        //     itemWidth: 103,
-        //     itemHeight: 20,
-        //     itemsSpacing: 5,
-        //     symbolSize: 10,
-        //     itemDirection: "left-to-right",
-        //     symbolShape: "circle",
-        //     effects: [
-        //       {
-        //         on: "hover",
-        //         style: {
-        //           itemTextColor: "#f77c08",
-        //         },
-        //       },
-        //     ],
-        //     itemTextColor: "white",
-        //   },
-        // ]}
-      />
-      <div className="chart-legend">
-      <p>{dotTotal }{"\u00A0"}<span>{localStorage.getItem("executionsTotal")}</span> {"\u00A0"} total executions </p>
-      <p>{dotToday }{"\u00A0"} <span>{localStorage.getItem("executionsToday")}</span>{"\u00A0"} today's executions</p>
+      <div className="chart-wrapper">
+        <ResponsiveRadialBar
+          data={data}
+          // centerValue={{
+          //   value: 2500,
+          //   formatted: true
+          // }}
+          // legendLabel={(x)=>${x.value}}
+          //  valueFormat="0=-0.0%"
+          // tooltip={(data) => <div className="tooltip">{data.bar.data.y}</div>}
+          maxValue={maxValue}
+          startAngle={-90}
+          endAngle={90}
+          // endAngle={275}
+          innerRadius={0.25}
+          padding={0.4}
+          padAngle={0}
+          margin={{ top: 40 }}
+          // margin={{ top: 40, right: 120, bottom: 40, left: 40 }}
+          colors={{ scheme: "nivo" }}
+          borderColor={{
+            from: "color",
+            modifiers: [["darker", "25"]],
+          }}
+          //   theme={{
+          //     dots: {
+          //         text: {
+          //             fill: '#ffffff',
+          //         },
+          //     },
+          // }}
+          enableTracks={true}
+          //legende of bars
+          // radialAxisStart={{ tickSize: 20, tickPadding: 9, tickRotation: 0 }}
+          // circularAxisOuter={{ tickSize: 5, tickPadding: 5, tickRotation: 0 }}
+          enableLabels={false}
+          label="value"
+          labelsTextColor={{ theme: "labels.text.fill" }}
+          enableRadialGrid={false}
+          enableCircularGrid={false}
+          radialAxisStart={false}
+          motionConfig="gentle"
+          isInteractive={true}
+          theme={theme}
+          // tooltip={({ bar }) => {
+          //   return (
+          //     <BasicTooltip
+          //       value={`${bar.value}`}
+          //       color={bar.color}
+          //       id={
+          //         <span>
+          //           {bar.category} - {bar.groupId}
+          //         </span>
+          //       }
+          //       enableChip
+          //     />
+          //   );
+          // }}
+
+          layers={["grid", "tracks", "bars", "labels", "legends", "CenteredMetric"]}
+          // legends={[
+          //   {
+          //     dataFrom: "keys",
+          //     // data: keys.map((item,index)=>({
+          //     //   id: item.id,
+          //     //   label:item.y
+          //     // })),
+          //     anchor: "right",
+          //     direction: "column",
+          //     justify: false,
+          //     translateX: 60,
+          //     //  translateY: -17,
+          //     itemWidth: 103,
+          //     itemHeight: 20,
+          //     itemsSpacing: 5,
+          //     symbolSize: 10,
+          //     itemDirection: "left-to-right",
+          //     symbolShape: "circle",
+          //     effects: [
+          //       {
+          //         on: "hover",
+          //         style: {
+          //           itemTextColor: "#f77c08",
+          //         },
+          //       },
+          //     ],
+          //     itemTextColor: "white",
+          //   },
+          // ]}
+        />
+        <div className="chart-legend">
+          <p>
+            {dotTotal}
+            {"\u00A0"}
+            <span>{counter ? counter.first : ""}</span> {"\u00A0"} {legend ? legend.first : ""}{" "}
+          </p>
+          <p>
+            {dotToday}
+            {"\u00A0"} <span>{counter ? counter.second : ""}</span>
+            {"\u00A0"} {legend ? legend.second : ""}
+          </p>
+        </div>
       </div>
-
-    </div>
     </div>
   );
 };
